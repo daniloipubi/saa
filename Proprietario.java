476ea9b4228b7f808651074120d55b7714252f5d
Danilo <danilo@DaniloPC.(none)>
2014-05-25 11:50:35 -0300

    Primeiro commit"

---
+++ Proprietario.java
@@ -0,0 +1,33 @@
+class Proprietario{
+
+private String nome;
+private boolean companhiaAerea;
+
+
+public Proprietario(String name, boolean ehCompanhia){
+
+	this.nome = name;
+	airCompany = ehCompanhia;
+
+}
+
+
+public void setProprietario(String algumNome){
+
+	nome = algumNome;
+}
+
+
+public String getProprietario(void){
+	
+	return nome;
+}
+
+
+public boolean isAirCompany(){
+
+	return airCompany;
+}
+
+
+
